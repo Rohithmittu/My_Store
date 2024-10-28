8019b992f3804d6e5118a591f58ebc25712d5246
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2024-10-28 21:04:53 +0530

    Coupon routes and controllers finished

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import couponRoutes from "./routes/coupon.route.js";
 
 dotenv.config();
 
@@ -18,6 +18,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:` + PORT);
